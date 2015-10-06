f2ae97e44111ef94e20fad037e0172ba89116e0e
RazvanHidan <razvan_hidan@yahoo.com>
2015-10-06 13:24:45 +0300

    Sportiv

---
+++ Sportiv/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sportiv
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(repetari(3), 9);
+            Assert.AreEqual(repetari(10), 100);
+            Assert.AreEqual(repetari(4), 16);
+        }
+        int repetari(int n)
+        {
+            return n * n;
+        }
+    }
+}
