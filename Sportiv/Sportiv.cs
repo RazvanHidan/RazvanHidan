2bf2152209d41f6dc110ac1c373c15231ae0df05
RazvanHidan <razvan_hidan@yahoo.com>
2015-10-07 12:16:45 +0300

    Probeme.1

---
+++ Sportiv/Sportiv.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sportiv
+{
+    [TestClass]
+    public class Sportiv
+    {
+        [TestMethod]
+        public void Repetition()
+        {
+            Assert.AreEqual(Repetition (3), 9);
+            Assert.AreEqual(Repetition (10), 100);
+            Assert.AreEqual(Repetition (4), 16);
+        }
+        int Repetition(int n)
+        {
+            return n * n;
+        }
+    }
+}
